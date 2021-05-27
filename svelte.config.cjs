24b40ff5f158059fe6586adb40a480f66568d863
Elton Lin <username@mit.edu>
2021-05-26 20:36:32 -0400

    Cleanup

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,11 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess')
 
 module.exports = {
-	// preprocess: [
-	// 	sveltePreprocess({
-	// 		postcss: true
-	// 	}),
-	// ],
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess({ postcss: true })
