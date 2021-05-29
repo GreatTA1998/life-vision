d4471456d8cb5516f6e95c82c9845b1f3e32bc1a
Elton Lin <username@mit.edu>
2021-05-29 16:32:35 -0400

    Add the elationships and environment category, highlight critical numbers in red

--- src/lib/PricingsDemo.svelte
+++ src/lib/PricingsDemo.svelte
@@ -2,8 +2,27 @@
   import supabase from '../supabase.js'
   import { onMount } from 'svelte';
 
+  // TODO: use Plaid to fetch the data
+  let accountBalance = 4000; 
+  let numOfFunActivitiesPerWeek = 0; 
+  let numOfDatesPerMonth = 0; 
+  let numOfLettersPerYear = 1; 
+
   let roomPhotoObjectURL
   onMount(async () => {
+    // get information about the room photos bucket first
+    const { data, error } = await supabase
+      .storage
+      .getBucket('photos')
+    console.log('data1 =', data)
+    console.log('error1 =', error)
+
+    const bucketsRequest = await supabase
+      .storage
+      .listBuckets()
+    console.log("buckets =", bucketsRequest.data)
+    console.log('error =', bucketsRequest.error)
+
     async function downloadExampleFile () {
       console.log('downloadExampleFile()')
       const { data, error } = await supabase
@@ -40,12 +59,15 @@
 </script>
 
 
+
+
 <section class="text-gray-600 body-font overflow-hidden">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Life Dashboard</h1>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Metrics that turn out to correlate with how good/bad life is so you know what to do</p>
     </div>
+
     <div class="flex flex-wrap -m-4">
       <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
         <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
@@ -70,15 +92,9 @@
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
-            </span>Friends: 0 activities/week
+            </span>Appearance (take a photo)
           </p>
-          <p class="flex items-center text-gray-600 mb-6">
-            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Dates: 0 dates/month
-          </p>
+
           <!-- Drink water, meditate etc. will be included in the detailed dashboard -->
           <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">More info
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
@@ -99,7 +115,7 @@
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
-            </span>Tidiness: (take a photo)
+            </span>Tidiness:
 
             <!-- Upload a photo -->
             <button on:click={fileUploader.click()}
@@ -116,22 +132,17 @@
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
-            </span>Supplies: need shampoo
+            </span>Bed comfort: OK
           </p>
+
           <p class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
-            </span>Friends: 0 activities/week
+            </span>Supplies: OK
           </p>
-          <p class="flex items-center text-gray-600 mb-6">
-            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>Dates: 0 dates/month
-          </p>
+
           <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">More info
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -145,7 +156,7 @@
           <!-- <span class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span> -->
           <h2 class="text-sm tracking-widest title-font mb-1 font-medium">My Finance</h2>
           <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-            <span>$4000</span>
+            <span class="{accountBalance < 3000 * 6 ? 'text-red-600' : ''}">${accountBalance}</span>
             <span class="text-lg ml-1 font-normal text-gray-500">account balance</span>
           </h1>
           <p class="flex items-center text-gray-600 mb-2">
@@ -176,9 +187,82 @@
           </button>
         </div>
       </div>
-      
-      
 
+      <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+        <div class="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden">
+          <!-- <span class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span> -->
+          <h2 class="text-sm tracking-widest title-font mb-1 font-medium">My Environment</h2>
+          <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+            <span class="text-red-600">0</span>
+            <span class="text-lg ml-1 font-normal text-gray-500">peers</span>
+          </h1>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Peers
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Mentors/Role models
+          </p>
+          <p class="flex items-center text-gray-600 mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Internet resources
+          </p>
+          <button class="flex items-center mt-auto text-white bg-indigo-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">More info
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </div>   
+
+      <!-- My relationships -->
+      <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+        <div class="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden">
+          <!-- <span class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span> -->
+          <h2 class="text-sm tracking-widest title-font mb-1 font-medium">My Relationships</h2>
+          <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+            <span class="{numOfFunActivitiesPerWeek === 0 ? 'text-red-600' : ''}">{ numOfFunActivitiesPerWeek }</span>
+            <span class="text-lg ml-1 font-normal text-gray-500">fun activities/week</span>
+          </h1>
+          <p class="flex items-center { numOfFunActivitiesPerWeek < 2 ? 'text-red-600' : 'text-gray-600' } mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Friends: { numOfFunActivitiesPerWeek } activities/week
+          </p>
+          <p class="flex items-center { numOfLettersPerYear < 2 ? 'text-red-600' : 'text-gray-600'} mb-2">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>
+            Letters: { numOfLettersPerYear } letters/year
+          </p>
+          <p class="flex items-center { numOfDatesPerMonth < 2 ? 'text-red-600' : 'text-gray-600' } mb-6">
+            <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>Dates: { numOfDatesPerMonth } dates/month
+          </p>
+          <button class="flex items-center mt-auto text-white bg-indigo-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">More info
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </div>   
     </div>
   </div>
 </section>
